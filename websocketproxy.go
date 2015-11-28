0bf9b6d9471d0cae5c5b9c700860fec777b94f66
Fabian Ruff <fabian@progra.de>
2015-11-28 14:07:54 +0100

    Avoid setting empty Origin header on backend connection

--- websocketproxy.go
+++ websocketproxy.go
@@ -86,7 +86,9 @@ func (w *WebsocketProxy) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	// Pass headers from the incoming request to the dialer to forward them to
 	// the final destinations.
 	requestHeader := http.Header{}
-	requestHeader.Add("Origin", req.Header.Get("Origin"))
+	if origin := req.Header.Get("Origin"); origin != "" {
+		requestHeader.Add("Origin", origin)
+	}
 	for _, prot := range req.Header[http.CanonicalHeaderKey("Sec-WebSocket-Protocol")] {
 		requestHeader.Add("Sec-WebSocket-Protocol", prot)
 	}
